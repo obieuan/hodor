3b95dc4a568ab09d43ab195c6a8a0e956b5cabca
obieuan <obieuan@gmail.com>
2022-06-04 19:08:46 -0500

    update

--- level_0/level1.py
+++ level_0/level1.py
@@ -8,6 +8,6 @@ import sys
 url = "http://158.69.76.135/level0.php"
 id = "4291"
 
-for i in range(0, 1024):
+for i in range(1, 1024):
     data = {'id': id, 'holdthedoor': 'Submit'}
     r = requests.post(url, data=data)
