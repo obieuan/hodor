f04939bf3cd8e57eb1b347319913b63493fa17f1
obieuan <obieuan@gmail.com>
2022-06-05 22:04:41 -0500

    update

--- level_2/level2.py
+++ level_2/level2.py
@@ -5,15 +5,14 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "http://158.69.76.135/level2.php"
-user_agent = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) "
-              "Gecko/20100101 Firefox/64.0")
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:69.0) Gecko/20100101 Firefox/69.0'
 header = {
     "User-Agent": user_agent,
     "referer": url
 }
 datos = {
     "id": "4291",
-    "submit": "holdthedoor",
+    "holdthedoor": "submit",
     "key": ""
 }
 contador = 1024
